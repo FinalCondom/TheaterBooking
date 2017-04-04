4cc7421b99defa0839279fe3d71ce83220333503
lucien zuber <lucien zuber>
2017-04-04 09:45:57 +0200

    ClassCreation

---
+++ app/src/main/java/com/example/lucien/theaterbooking/DisplayMovieActivity.java
@@ -0,0 +1,10 @@
+package com.example.lucien.theaterbooking;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lucien on 04.04.2017.
+ */
+
+public class DisplayMovieActivity extends AppCompatActivity {
+}
