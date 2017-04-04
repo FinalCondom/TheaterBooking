48b2ab5ccb32b572951a5b7af701df5e0268d885
lucien zuber <lucien zuber>
2017-04-04 08:49:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lucien/theaterbooking/DisplayTheaterActivity.java
@@ -0,0 +1,13 @@
+package com.example.lucien.theaterbooking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayTheaterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_theater);
+    }
+}
