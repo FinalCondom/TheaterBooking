a9672cb920b3d6d7643acbd3a7bdf51a325a946e
Robert <robertleeseligmann@gmail.com>
2017-04-19 14:03:07 +0200

    SQL Lite Database Implemented 11.04.2017

--- app/src/main/java/com/example/lucien/theaterbooking/DisplayMovieActivity.java
+++ app/src/main/java/com/example/lucien/theaterbooking/DisplayMovieActivity.java
@@ -1,9 +1,15 @@
 package com.example.lucien.theaterbooking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.lucien.theaterbooking.DatabaseObject.Movie;
+import com.example.lucien.theaterbooking.DatabaseTable.DatabaseHandler;
 
 /**
  * Created by lucien on 04.04.2017.
@@ -12,7 +18,23 @@ import android.support.v7.app.AppCompatActivity;
 public class DisplayMovieActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate( Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_movie);
+
+Log.d("Hello","world");
+
+        int id=getIntent().getIntExtra("movieId",0);
+
+        Movie m=DisplayTheaterActivity.db.getMovie(id);
+
+        Log.d("Title",m.getTitle());
+        TextView title=(TextView) findViewById(R.id.singleMovieTite);
+        TextView desc=(TextView) findViewById(R.id.singleMovieDesc);
+        TextView duration=(TextView) findViewById(R.id.singleMovieDur);
+
+        title.setText(m.getTitle());
+        desc.setText("Description: "+m.getDescription());
+        duration.setText("Duration: "+m.getDuration());
     }
 }
--- app/src/main/java/com/example/lucien/theaterbooking/DisplayTheaterActivity.java
+++ app/src/main/java/com/example/lucien/theaterbooking/DisplayTheaterActivity.java
@@ -1,9 +1,11 @@
 package com.example.lucien.theaterbooking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,42 +17,40 @@ import com.example.lucien.theaterbooking.DatabaseObject.Show;
 import com.example.lucien.theaterbooking.DatabaseTable.DatabaseHandler;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class DisplayTheaterActivity extends AppCompatActivity {
         ListView mListView ;
+    public static DatabaseHandler db ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db= new DatabaseHandler(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_theater);
         DatabaseHandler db = new DatabaseHandler(this);
-        mListView = (ListView) findViewById(R.id.listView);
+       setFirstData(db);
 
+        ArrayList<Movie> allMovies=(ArrayList<Movie>) db.getAllMovies();
+        final ListView lv1 = (ListView) findViewById(R.id.movie_list);
+        lv1.setAdapter(new MovieAdapter(this, allMovies));
+       lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent =new Intent(DisplayTheaterActivity.this,DisplayMovieActivity.class);
+               Movie m = (Movie)lv1.getItemAtPosition(position);
+               intent.putExtra("movieId",m.getIdMovie());
+               startActivity(intent);
+           }
+       });
 
-
-        //setFirstData(db);
-
-
-
-        /*Show tempShow = db.getShow(1);
-        Log.d("Idmovie",String.valueOf(tempShow.getIdMovie()));
-        Movie tempMove = db.getMovie(tempShow.getIdMovie());
-        Actor tempAct=db.getActor(1);
-
-
-        Booking tempbook = db.getBooking(1);
-
-        TextView test = (TextView) findViewById(R.id.displayname);
-        test.setText(tempShow.getDate() + " / " + tempbook.getClientName()+" / "+ tempMove.getDescription()+" / "+tempAct.getActorName());*/
     }
 
     private void setFirstData(DatabaseHandler db) {
         Log.d("Insert", "Inserting actor");
 
-        Room arlequin = new Room("Arlequin", "", 334);
 
 
 
+        db.AddRoom(new Room("Arlequin", "", 334));
 
         //adding actor
 
@@ -59,12 +59,14 @@ public class DisplayTheaterActivity extends AppCompatActivity {
         db.AddActor( new Actor("Chris Pratt"));
 
         //adding movies
-        db.AddMovie( new Movie("John Wick : Chapter 2", "122 minutes"));
-        db.AddMovie(new Movie("Guardians of the Galaxy 2", "120 minutes"));
+        db.AddMovie( new Movie("John Wick : Chapter 2", "John Wick est forcé de sortir de sa retraite volontaire par un de ses ex-associés qui cherche à prendre le contrôle d’une mystérieuse confrérie de tueurs internationaux. Parce qu’il est lié à cet homme par un serment, John se rend à Rome, " +
+                "où il va devoir affronter certains des tueurs les plus dangereux du monde. ","122 minutes"));
+        db.AddMovie(new Movie("Guardians of the Galaxy 2","Les Gardiens de la galaxie 2 poursuit les aventures de l'équipe alors qu'elle traverse les confins du cosmos. Les gardiens doivent combattre pour " +
+                "rester unis alors qu'ils découvrent les mystères de la filiation de Peter Quill", "120 minutes"));
 
         //Adding room
 
-        db.AddRoom(arlequin);
+
         db.AddRoom(new Room("Lux", "dont 16 sièges VIP, boucle magnétique pour malentendants", 198));
         db.AddRoom(new Room("Capitole", "", 110));
         db.AddRoom(new Room("Les cèdres", "", 80));
@@ -74,7 +76,9 @@ public class DisplayTheaterActivity extends AppCompatActivity {
         db.AddActorInMovie(db.getActor(2),db.getMovie(1));
         db.AddActorInMovie(db.getActor(3),db.getMovie(2));
 
-        Show jw1 = new Show(arlequin.getIdRoom(), db.getMovie(1).getIdMovie(), 20, arlequin.getNumberSeats(), "Jeudi 13 avril 2017 ");
+
+        Room room=db.getRoom(2);
+        Show jw1 = new Show(room.getIdRoom(), db.getMovie(1).getIdMovie(), 20, room.getNumberSeats(), "Jeudi 13 avril 2017 ");
 
         db.AddShow(jw1);
 
